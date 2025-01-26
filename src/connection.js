a677cba25c48958e45a0f29af2d416a4eb998ffa
Harsh Dewangan <harshkumardewangan2003@gmail.com>
2025-01-27 01:53:15 +0530

    Update connection.js

--- src/connection.js
+++ src/connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'chat',
+    host: 'sql307.infinityfree.com',
+    user: 'if0_38183213',
+    password: 'NI3hmsnuOXy',
+    database: 'if0_38183213_XXX',
     multipleStatements: true
 });
 conn.connect(function(error){
