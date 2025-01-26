c0891426bbdfb1c37b53764b5e1c8544a6077dd1
Harsh Dewangan <harshkumardewangan2003@gmail.com>
2025-01-27 02:09:14 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -87,6 +87,22 @@ app.use(bodyParser.json());
 
 
 
+const conn = mysql.createConnection({
+    host: 'sql307.infinityfree.com',
+    user: 'if0_38183213',
+    password: 'NI3hmsnuOXy',
+    database: 'if0_38183213_XXX',
+    multipleStatements: true
+});
+conn.connect(function(error){
+    if(error)
+    {
+        throw error;
+    }
+    else{
+        console.log("Connected To Database")
+    }
+});
 
 app.get('/home/pdf/:id/list', (req, res) => {
   if (req.session.loggedin) {
